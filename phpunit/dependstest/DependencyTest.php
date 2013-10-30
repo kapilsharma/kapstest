6ab12589affb56d3b918a781c370dda708121ab4
kapilsharma <kapil@kapilsharma.info>
2013-10-30 12:56:58 +0530

    Few updated example test cases

---
+++ phpunit/dependstest/DependencyTest.php
@@ -0,0 +1,85 @@
+<?PHP
+
+/**
+ * Dependency test\
+ * @group all
+ * @group dependency
+ * @group array
+ */
+class DependencyTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Simple method initiating array and testing array is empty
+     * @group array_empty
+     * $group array_push
+     * @group array_pop
+     */ 
+    public function testArrayEmpty()
+    {
+        $stack = array();
+
+        $this->assertTrue(empty($stack));
+
+        return $stack;
+    }
+
+    /**
+     * Test to push data in array.
+     * @group array_push
+     * @depends testArrayEmpty
+     */
+    public function testPush($array)
+    {
+        array_push($array, 'foo');
+
+        $this->assertFalse(empty($array));
+
+        return $array;
+    }
+
+    /**
+     * Test to pop data
+     * @group array_pop
+     * @depends testPush
+     */
+    public function testPop($array)
+    {
+        $this->assertFalse(empty($array));
+
+        $data = array_pop($array);
+
+        $this->assertEquals('foo', $data);
+        $this->assertTrue(empty($stack));
+    }
+
+    /**
+     * Test to puch data in array.
+     * @group array_push
+     * @depends testArrayEmpty
+     */
+    public function testPush2($array)
+    {
+        array_push($array, 'foo');
+
+        $this->assertFalse(empty($array));
+
+        return $array;
+    }
+
+    /**
+     * Test to pop data
+     * @group array_pop
+     * @depends testPush2
+     */
+    public function testPop2($array)
+    {
+        $this->assertFalse(empty($array));
+
+        $data = array_pop($array);
+
+        $this->assertEquals('foo', $data);
+        $this->assertTrue(empty($stack));
+    }
+
+}
--- phpunit/dependstest/test1.php
+++ phpunit/dependstest/test1.php
@@ -1,10 +1,14 @@
 <?PHP
+
+/**
+ * A simple dummy test.
+ */
 class test1 extends PHPUnit_Framework_TestCase
 {
     /**
      * First test
      * @group all
-     * @group test1
+     * @group dummy
      */
     function testDummy1()
     {
