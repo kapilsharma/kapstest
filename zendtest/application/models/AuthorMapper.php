63fe5e95a04007a38adddcd74a6eeb021fda0332
kapilsharma <kapil@kapilsharma.info>
2013-11-01 17:04:31 +0530

    Model files, not completed

---
+++ zendtest/application/models/AuthorMapper.php
@@ -0,0 +1,73 @@
+<?php
+
+class Application_Model_AuthorMapper
+{
+
+    protected $_dbTable;
+ 
+    public function setDbTable($dbTable)
+    {
+        if (is_string($dbTable)) {
+            $dbTable = new $dbTable();
+        }
+        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+            throw new Exception('Invalid table data gateway provided');
+        }
+        $this->_dbTable = $dbTable;
+        return $this;
+    }
+ 
+    public function getDbTable()
+    {
+        if (null === $this->_dbTable) {
+            $this->setDbTable('Application_Model_DbTable_Author');
+        }
+        return $this->_dbTable;
+    }
+ 
+    public function save(Application_Model_Author $author)
+    {
+        $data = array(
+            'email'   => $author->getEmail(),
+            'comment' => $author->getComment(),
+            'created' => date('Y-m-d H:i:s'),
+        );
+ 
+        if (null === ($id = $author->getId())) {
+            unset($data['id']);
+            $this->getDbTable()->insert($data);
+        } else {
+            $this->getDbTable()->update($data, array('id = ?' => $id));
+        }
+    }
+ 
+    public function find($id, Application_Model_Author $author)
+    {
+        $result = $this->getDbTable()->find($id);
+        if (0 == count($result)) {
+            return;
+        }
+        $row = $result->current();
+        $author->setId($row->id)
+                  ->setEmail($row->email)
+                  ->setComment($row->comment)
+                  ->setCreated($row->created);
+    }
+ 
+    public function fetchAll()
+    {
+        $resultSet = $this->getDbTable()->fetchAll();
+        $entries   = array();
+        foreach ($resultSet as $row) {
+            $entry = new Application_Model_Author();
+            $entry->setId($row->id)
+                  ->setEmail($row->email)
+                  ->setComment($row->comment)
+                  ->setCreated($row->created);
+            $entries[] = $entry;
+        }
+        return $entries;
+    }
+
+}
+
---
+++ zendtest/application/models/DbTable/Author.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Author extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'author';
+
+
+}
+
